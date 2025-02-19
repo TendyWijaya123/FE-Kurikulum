9eea2cc7e483a0d35f3e106e4cabb0c5704b3384
unknown <mahkotonagata@gmail.com>
2025-02-19 14:31:47 +0700

    fix: update ipteks

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,12 @@
+import DefaultLayout from "../layouts/DefaultLayout"
+
+const Dashboard = () => {
+    return (
+        <DefaultLayout>
+            <h1>Dashboard</h1>
+        </DefaultLayout>
+    );
+    
+};
+
+export default Dashboard;
